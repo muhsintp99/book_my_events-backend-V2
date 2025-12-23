4f979fee204139b41ee19bf23590f1a5ad2af33e
Sreerag o s <sreeragsnair142@gmail.com>
2025-12-23 11:38:31 +0530

    add

--- server.js
+++ server.js
@@ -462,6 +462,7 @@ const allowedOrigins = [
   "http://localhost:5002",
   "http://localhost:5173",
   "http://127.0.0.1:5500",
+  "http://127.0.0.1:5501",
 
   // Dashboards
   "https://vendor.bookmyevent.ae",
