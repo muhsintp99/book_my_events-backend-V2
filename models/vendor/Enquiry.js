6bca4681ec81e01e657846e499e14d24cea97e39
Sreerag o s <sreeragsnair142@gmail.com>
2026-02-11 05:01:56 +0530

    venue multiple image add

--- models/vendor/Enquiry.js
+++ models/vendor/Enquiry.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const enquirySchema = new mongoose.Schema(
   {
     userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+  type: mongoose.Schema.Types.ObjectId,
+  ref: "User",
+  default: null,
+},
+
 
     vendorId: {
       type: mongoose.Schema.Types.ObjectId,
